ccb39d93b954e8223229e13a1989b12e0e0be7f9
ltkv12 <juliako20062018@gmail.com>
2026-04-04 20:42:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,8 +40,8 @@ class AdminStates(StatesGroup):
 BRAVECTO_TABLETS_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/bravecto_tablets.jpg"
 BRAVECTO_DROPS_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/bravecto_drops.jpg"
 SIMPARICA_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/simparica.jpg"
-SIMPARICA_TRIO_PHOTO = "https://github.com/ltkv12/telegram-bot/blob/main/images/simparica%20trio.jpg"
-TIXFLI_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/simparica%20trio.jpg"
+SIMPARICA_TRIO_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/simparica%20trio.jpg"
+TIXFLI_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/tixfli.jpg"
 
 # ========== ТОВАРЫ ==========
 BRAVECTO_TABLETS = [
