e87444f9775d12fba8c337318d8b36ee80061e40
ltkv12 <juliako20062018@gmail.com>
2026-04-04 20:32:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ class AdminStates(StatesGroup):
 BRAVECTO_TABLETS_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/bravecto_tablets.jpg"
 BRAVECTO_DROPS_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/bravecto_drops.jpg"
 SIMPARICA_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/simparica.jpg"
-SIMPARICA_TRIO_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/simparica.jpg"
+SIMPARICA_TRIO_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/%D1%81%D0%B8%D0%BC%D0%BF%D0%B0%D1%80%D0%B8%D0%BA%D0%B0%20%D1%82%D1%80%D0%B8%D0%BE.jpg"
 TIXFLI_PHOTO = "https://raw.githubusercontent.com/ltkv12/telegram-bot/main/images/tixfli.jpg"
 
 # ========== ТОВАРЫ ==========
@@ -612,7 +612,6 @@ async def catalog(call: CallbackQuery):
     await call.message.delete()
     await call.answer()
 
-# ========== ОСНОВНЫЕ ФУНКЦИИ ПОКАЗА ТОВАРОВ (ИСПРАВЛЕНЫ) ==========
 @dp.callback_query(F.data == "show_bravecto_tablets")
 async def show_bravecto_tablets(call: CallbackQuery):
     text = "*🟢 Бравекто (таблетки) / Bravecto (tablets)*\n\n"
